388cbec4bf2e8ea59843b4cfd1d154e9b0b4805e
Alejandro Almanza <almanza.marfrancisco@hotmail.com>
2023-09-08 12:48:46 -0600

    Finished

--- main.py
+++ main.py
@@ -37,10 +37,7 @@ def AESdecipher(ciphertext, key):
 def makePairOfKeys():
     if not os.path.exists(f'./rsa'):
         os.makedirs(f'./rsa')
-    if os.path.exists(f"./rsa/a_pubkey_rsa") and os.path.exists(f"./rsa/a_privkey_rsa.pem") and os.path.exists(f"./rsa/b_pubkey_rsa") and os.path.exists(f"./rsa/b_privkey_rsa.pem"):
-        print(f"\t\t-> key files found, skipping creation...")
-        return
-    try:
+        print("=> rsa folder not found. Generating keys ...")
         # RSA with signature
         keyPair = RSA.generate(bits=1024)
         with open(f"./rsa/a_pubkey_rsa", "w+") as pubfile:
@@ -49,7 +46,6 @@ def makePairOfKeys():
         with open(f"./rsa/a_privkey_rsa.pem", "w+") as privfile:
             privfile.write(f"-----RSA Private Key-----\n{hex(keyPair.d)}\n\n{keyPair.n}\n-----RSA Private Key END-----")
             print(f"\t\t=> A privkey generated on rsa generated")
-
         # RSA without signature
         (b_pubkey, b_privkey) = rsa.newkeys(512)
         with open(f"./rsa/b_pubkey_rsa", "wb+") as pubfile:
@@ -58,9 +54,7 @@ def makePairOfKeys():
         with open(f"./rsa/b_privkey_rsa.pem", "wb+") as privfile:
             privfile.write(b_privkey.save_pkcs1())
             print(f"\t\t=> B privkey generated on rsa generated")
-    except FileExistsError:
-        print("\t Something went really wrong! x(")
-    print(f"\t\tDone!")
+    
 
 class windowLayout:
     def __init__(self) -> None:
@@ -82,13 +76,10 @@ class windowLayout:
         self.switch_button = Button(mainframe, text="Encrypt", bd=0, command=self.switch)
         self.switch_button.grid(column=0, row=2)
 
-        # Checkboxes
+        # Checkbox
         self.sign_value = IntVar()
-        self.verification_value = IntVar()
-        self.sing_check = Checkbutton(mainframe, text = "Signing", variable = self.sign_value,onvalue = 1, offvalue = 0, height=1, width = 10)
-        self.sign_verification = Checkbutton(mainframe, text = "Varification", variable = self.verification_value, onvalue = 1, offvalue = 0, height=1, width = 10)
-        self.sing_check.grid(column=0, row=3)
-        self.sign_verification.grid(column=0, row=4)
+        self.sing_check = Checkbutton(mainframe, text = "Signing/Verification", variable = self.sign_value,onvalue = 1, offvalue = 0, height=1, width = 20)
+        self.sing_check.grid(column=0, row=3, columnspan=10)
 
         # Key File selection
         self.key_file_selection = Button(mainframe, text="Select the key file", bd=0, command=partial(self.select_file,"key"))
@@ -141,7 +132,6 @@ class windowLayout:
             self.key_filename_text.set(filename)
             
     def action(self, *args):
-        # print(f"Variables values: \n\tSwitchButton: {self.action_switch} \n\tSigning: {self.sign_value.get()}, \n\tVer: {self.verification_value.get()}")
         if self.action_switch: # Encrypt
             self.encrypt(self.filename_text.get())
         else: # Decrypt
@@ -149,60 +139,89 @@ class windowLayout:
 
     def encrypt(self, filename, *args):
         AES_key = getRandomBytes(16)
+        self.infoBox.insert(END, "[I] Obtaining data file ...\n")
         with open(filename, mode='rb') as file:
             data = file.read()
         # AES Ciphering
+        self.infoBox.insert(END, "[I] Ciphering with AES ...\n")
         cipher_text = AEScipher(getRandomBytes(16)+data, AES_key) # Padding of 16 bytes
         # RSA Encryption for AES Key
-        with open(f'./rsa/b_pubkey_rsa', mode='rb') as pubfile: # Bert Public key selected by default
+        self.infoBox.insert(END, "[I] Obtaining public key ... \n")
+        with open(f'./rsa/b_pubkey_rsa', mode='rb') as pubfile: # Public key selected by default
             keydata = pubfile.read()
         pubkey = rsa.PublicKey.load_pkcs1(keydata)
+        self.infoBox.insert(END, "[I] Ciphering with RSA ...\n")
         ciphered_key = rsa.encrypt(AES_key, pubkey)
         # RSA Encryption for digital sign
-        print("\nLet's take 32 elements\n",data[:32])
-        hash = int.from_bytes(sha512(data[:32]).digest(), byteorder='big')
-        with open(self.key_filename_text.get(), mode='r') as privfile: # User select private key
-            keydata = privfile.read()
-        n_start = keydata.find("\n\n")+2
-        n = int(keydata[n_start:keydata.find("-----RSA Private Key END-----")], 16)
-        d = int(keydata[keydata.find("\n\n") -1:keydata.find("\n\n")], 16)
-        signature = pow(hash, d, n)
-        with open(f"./signature", "w+") as signaturefile:
-            signaturefile.write(f"{hex(signature)}")
+        if self.sign_value.get():
+            # print("\nLet's take 32 elements\n",data[:32])
+            hash = int.from_bytes(sha512(data[:32]).digest(), byteorder='big')
+            self.infoBox.insert(END, "[I] Obtaining privatekey for singnature ... \n")
+            with open(self.key_filename_text.get(), mode='r') as privfile: # User select private key
+                keydata = privfile.read()
+            n_start = keydata.find("\n\n")+2
+            n = int(keydata[n_start:keydata.find("-----RSA Private Key END-----")], 16)
+            d = int(keydata[keydata.find("\n\n") -1:keydata.find("\n\n")], 16)
+            signature = pow(hash, d, n)
+            self.infoBox.insert(END, "[I] Singning file ...\n")
+            with open(f"./signature", "w+") as signaturefile:
+                signaturefile.write(f"{hex(signature)}")
+            self.infoBox.insert(END, "[I] Done!\n")
         with open(f"./output", "wb+") as outputfile:
             outputfile.write(ciphered_key+b"\n\n"+cipher_text)
-        self.infoBox.insert(END, "\n[I] Encryption Done!\n")
+        self.infoBox.insert(END, "[I] Encryption Done!\n\n\n")
         self.filename_text.set("")
         self.key_filename_text.set("")
 
     def decrypt(self, filename):
        # RSA decrypt for AES key
+        self.infoBox.insert(END, "[I] Obtaining private key file ...\n")
         with open(self.key_filename_text.get(), mode='rb') as privatefile:
             keydata = privatefile.read()
             privkey = rsa.PrivateKey.load_pkcs1(keydata)
         with open(self.filename_text.get(), mode='rb+') as cipheredfile:
             data = cipheredfile.read()
+        self.infoBox.insert(END, "[I] Deciphering with RSA...\n")
         ciphered_key = data[:data.find(b"\n\n")]
-        deciphered_key = rsa.decrypt(ciphered_key, privkey)
+        try:
+            deciphered_key = rsa.decrypt(ciphered_key, privkey)
+        except Exception as err:
+            messagebox.showerror('Error', f'Error: To decipher the key x(\n{err}')
+            self.infoBox.insert(END, f"[E] To decipher the key x(\n\n\t{err}\n")
+            return
         ciphered_text = data[data.find(b"\n\n")+2:]
         # AES decryption
-        plaintext = AESdecipher(ciphered_text, deciphered_key)
+        try:
+            plaintext = AESdecipher(ciphered_text, deciphered_key)
+        except Exception as err:
+            messagebox.showerror('Error', f'Error: To decipher the file x(\n{err}')
+            self.infoBox.insert(END, f"[E] To decipher the file x(\n\n\t{err}")
+            return
         plaintext = plaintext[16:] # padding from encryption
         print(plaintext)
+        self.infoBox.insert(END, "[I] Deciphering with AES...\n")
         with open(f"./plaintext.txt", "wb+") as outputfile:
             outputfile.write(plaintext)
         # RSA Decryption for digital sign
-        with open("./rsa/a_pubkey_rsa", mode='r+') as pubfile: # Alice Public key selected by default
-            keydata = pubfile.read()
-        n_start = keydata.find("\n\n")+2
-        n = int(keydata[n_start:keydata.find("-----RSA Public Key END-----")], 16)
-        e = int(keydata[keydata.find("\n\n") -1:keydata.find("\n\n")], 16)
-        with open("./signature", mode='r+') as signfile: # signature
-            signature_data = signfile.read()
-        hash = int.from_bytes(sha512(plaintext[:32]).digest(), byteorder='big')
-        hashFromSignature = pow(int(signature_data, 16), e, n)
-        print("Signature valid:", hash == hashFromSignature)
-
+        if self.sign_value.get():
+            try:
+                self.infoBox.insert(END, "[I] Verifying signature ...\n")
+                with open("./rsa/a_pubkey_rsa", mode='r+') as pubfile: # Alice Public key selected by default
+                    keydata = pubfile.read()
+                n_start = keydata.find("\n\n")+2
+                n = int(keydata[n_start:keydata.find("-----RSA Public Key END-----")], 16)
+                e = int(keydata[keydata.find("\n\n") -1:keydata.find("\n\n")], 16)
+                with open("./signature", mode='r+') as signfile: # signature
+                    signature_data = signfile.read()
+                hash = int.from_bytes(sha512(plaintext[:32]).digest(), byteorder='big')
+                hashFromSignature = pow(int(signature_data, 16), e, n)
+                print("Signature valid:", hash == hashFromSignature)
+                self.infoBox.insert(END, f"\n=> Signature valid: {hash == hashFromSignature}\n")
+            except Exception as err:
+                messagebox.showerror('Error', f'Error: To verify the signature x(\n{err}')
+                self.infoBox.insert(END, f"[E] To verify the signature x(\n\n\t{err}")
+        self.infoBox.insert(END, f"[I] Plaintext gotten: \n {plaintext}\n\n\n")
+        
 if __name__ == '__main__':
     print("\n\n**ETS Project**")
     init()
